314f837df63cef907ea3e39cc357227fbcfe88d9
hyeyeonie <shy020916@naver.com>
2025-10-12 15:29:08 +0900

    [init] 초기 세팅

---
+++ seminar_01/seminar_01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seminar_01
+//
+//  Created by 신혜연 on 10/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
